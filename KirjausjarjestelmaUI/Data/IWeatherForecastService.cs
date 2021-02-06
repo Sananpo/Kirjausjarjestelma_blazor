9b20178f4fad7454bd961429f625a3bf758e4b4e
Sampo Järvelä <sananpo1@gmail.com>
2021-02-06 17:16:23 +0200

    Add project files.

---
+++ KirjausjarjestelmaUI/Data/IWeatherForecastService.cs
@@ -0,0 +1,14 @@
+﻿using KirjausjarjestelmaDB.Data.BlazorApp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KirjausjarjestelmaUI.Data
+{
+    public interface IWeatherForecastService
+    {
+        Task<List<WeatherForecast>> GetForecastAsync(string strCurrentUser);
+        Task<WeatherForecast> CreateForecastAsync(WeatherForecast objWeatherForecast);
+        Task<bool> UpdateForecastAsync(WeatherForecast objWeatherForecast);
+        Task<bool> DeleteForecastAsync(WeatherForecast objWeatherForecast);
+    }
+}
---
+++ KirjausjarjestelmaUI/Data/WeatherForecastService.cs
@@ -0,0 +1,81 @@
+using KirjausjarjestelmaDB.Data.BlazorApp;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace KirjausjarjestelmaUI.Data
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly BlazorappDevContext context;
+
+        public WeatherForecastService(BlazorappDevContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<WeatherForecast>>
+            GetForecastAsync(string strCurrentUser)
+        {
+            // Get Weather Forecasts  
+            return await context.WeatherForecast
+                 // Only get entries for the current logged in user
+                 .Where(x => x.UserName == strCurrentUser)
+                 // Use AsNoTracking to disable EF change tracking
+                 // Use ToListAsync to avoid blocking a thread
+                 .AsNoTracking().ToListAsync();
+        }
+
+        public Task<WeatherForecast>
+            CreateForecastAsync(WeatherForecast objWeatherForecast)
+        {
+            context.WeatherForecast.Add(objWeatherForecast);
+            context.SaveChanges();
+            return Task.FromResult(objWeatherForecast);
+        }
+
+        public Task<bool>
+            UpdateForecastAsync(WeatherForecast objWeatherForecast)
+        {
+            var ExistingWeatherForecast =
+                context.WeatherForecast
+                .Where(x => x.Id == objWeatherForecast.Id)
+                .FirstOrDefault();
+            if (ExistingWeatherForecast != null)
+            {
+                ExistingWeatherForecast.Date =
+                    objWeatherForecast.Date;
+                ExistingWeatherForecast.Summary =
+                    objWeatherForecast.Summary;
+                ExistingWeatherForecast.TemperatureC =
+                    objWeatherForecast.TemperatureC;
+                ExistingWeatherForecast.TemperatureF =
+                    objWeatherForecast.TemperatureF;
+                context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task<bool>
+            DeleteForecastAsync(WeatherForecast objWeatherForecast)
+        {
+            var ExistingWeatherForecast =
+                context.WeatherForecast
+                .Where(x => x.Id == objWeatherForecast.Id)
+                .FirstOrDefault();
+            if (ExistingWeatherForecast != null)
+            {
+                context.WeatherForecast.Remove(ExistingWeatherForecast);
+                context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+    }
+}
